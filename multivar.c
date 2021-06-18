925c18029dd9b2175fb7717d87be52558807be5f
JYOTHULAJALASURYACHANDRAPAVANI <84400898+JYOTHULAJALASURYACHANDRAPAVANI@users.noreply.github.com>
2021-06-18 09:16:22 -0700

    Create multivar.c

---
+++ multivar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch='a';
+    char str[20]="welcome to c";
+    float flt=10.234;
+    int no=150;
+    double dbl=20.123456;
+    printf("%c\n",ch);
+    printf("%s\n",str);
+    printf("%f\n",flt);
+    printf("%d\n",flt);
+    printf("%lf\n",dbl);
+    return 0;
+}
